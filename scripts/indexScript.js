1b8f0b427fff0c8b341290cf019547e804a1a434
Noatak <noatak.post@gmail.com>
2022-09-29 14:32:44 -0700

    NEW MENU WORKS!

--- scripts/indexScript.js
+++ scripts/indexScript.js
@@ -2,14 +2,49 @@ var mini = true;
 
 function toggleSidebar() {
   if (mini) {
-    console.log("opening sidebar");
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
+    moveSocials();
     this.mini = false;
   } else {
-    console.log("closing sidebar");
     document.getElementById("mySidebar").style.width = "85px";
     document.getElementById("main").style.marginLeft = "85px";
+    moveSocials();
     this.mini = true;
   }
 }
+
+function moveSocials(){
+  if(mini){
+    document.getElementById("email").style.marginTop = "100px";
+    document.getElementById("insta").style.marginLeft = "80px";
+    document.getElementById("insta").style.marginTop = "-65px";
+    document.getElementById("twit").style.marginLeft = "148px";
+    document.getElementById("twit").style.marginTop = "-65px";
+
+    document.getElementById("myContacts").bottom = "80px";
+  }else {
+    document.getElementById("email").style.marginTop = "0"
+    document.getElementById("insta").style.marginLeft = "12px";
+    document.getElementById("insta").style.marginTop = "0";
+    document.getElementById("twit").style.marginLeft = "12px";
+    document.getElementById("twit").style.marginTop = "0";
+  }
+}
+
+
+/*function rotateSocials(){
+  if(mini){
+    document.getElementById("email").style.marginTop = "100px"
+    document.getElementById("insta").style.marginLeft = "80px";
+    document.getElementById("insta").style.marginTop = "-65px";
+    document.getElementById("twit").style.marginLeft = "148px";
+    document.getElementById("twit").style.marginTop = "-65px";
+  }else {
+    document.getElementById("email").style.marginTop = "0"
+    document.getElementById("insta").style.marginLeft = "12px";
+    document.getElementById("insta").style.marginTop = "0";
+    document.getElementById("twit").style.marginLeft = "12px";
+    document.getElementById("twit").style.marginTop = "0";
+  }
+}*/
